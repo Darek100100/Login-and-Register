be1c05aab3b8f83d2c0781b051399cd586de1eea
Darek <106867377+Darek100100@users.noreply.github.com>
2023-08-16 17:24:38 +0200

    Added class JwtService Extracted jwt and userEmail

--- src/main/java/com/example/security/user/config/JwtAutheticationFilter.java
+++ src/main/java/com/example/security/user/config/JwtAutheticationFilter.java
@@ -6,12 +6,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAutheticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
 
     @Override
     protected void doFilterInternal(
@@ -21,10 +25,15 @@ public class JwtAutheticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         if(authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);\
+            filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+        }
     }
 }
