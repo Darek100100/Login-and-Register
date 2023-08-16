b7a22abf53f2053dd7835d58e847139130bb20cf
Darek <106867377+Darek100100@users.noreply.github.com>
2023-08-16 15:08:21 +0200

    Started implementing filters

--- src/main/java/com/example/security/user/config/JwtAutheticationFilter.java
+++ src/main/java/com/example/security/user/config/JwtAutheticationFilter.java
@@ -19,6 +19,12 @@ public class JwtAutheticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);\
+            return;
+        }
+        jwt = authHeader.substring(7);
     }
 }
